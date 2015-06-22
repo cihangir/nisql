d95ee379b7c4db00449fb7c0ee0785aeb7408dc9
Cihangir SAVAS <cihangir@koding.com>
2015-06-22 03:43:11 -0700

    Nisq: added travis integration

---
+++ test_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo "Testing against mysql"
+export NISQL_TEST_DSN=nisqltest/nisqltest/nisqltest
+export NISQL_TEST_DIALECT=mysql
+go test ./...
+
+echo "Testing against postgres"
+export NISQL_TEST_DSN="user=nisqltest password=nisqltest dbname=nisqltest sslmode=disable"
+export NISQL_TEST_DIALECT=postgres
+go test ./...
+
+echo "Testing against sqlite"
+export NISQL_TEST_DSN=/tmp/nisqltest.bin
+export NISQL_TEST_DIALECT=sqlite
+go test ./...
