aef1dccad153f1cda4a79ac429dfc7a0b4482b3f
Cihangir SAVAS <cihangir@koding.com>
2015-06-22 04:19:41 -0700

    Nisq: remove sqlite package import

--- test_all.sh
+++ test_all.sh
@@ -10,8 +10,3 @@ echo "Testing against postgres"
 export NISQL_TEST_DSN="user=nisqltest password=nisqltest dbname=nisqltest sslmode=disable"
 export NISQL_TEST_DIALECT=postgres
 go test ./...
-
-echo "Testing against sqlite"
-export NISQL_TEST_DSN=/tmp/nisqltest.bin
-export NISQL_TEST_DIALECT=sqlite3
-go test ./...
