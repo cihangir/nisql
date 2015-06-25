404197405ea2ca3a063d4b273ca61113b6a0ab22
Cihangir SAVAS <cihangir@koding.com>
2015-06-25 04:34:12 -0700

    Nisql: added test cases for marshaling and unmarshaling

--- nisql_test.go
+++ nisql_test.go
@@ -2,7 +2,10 @@ package nisql
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -27,7 +30,10 @@ type nullable struct {
 }
 
 func TestInit(t *testing.T) {
-	db, err := sql.Open(os.Getenv("NISQL_TEST_DIALECT"), os.Getenv("NISQL_TEST_DSN"))
+	db, err := sql.Open(
+		os.Getenv("NISQL_TEST_DIALECT"),
+		os.Getenv("NISQL_TEST_DSN"),
+	)
 	if err != nil {
 		t.Fatalf("err while creating connection: %s", err.Error())
 	}
@@ -124,4 +130,127 @@ VALUES
 	if n.TimeVal.IsZero() {
 		t.Fatalf("expected valid, got invalid for TimeVal: %+v", n.TimeVal)
 	}
+
+	if _, err := db.Exec("DELETE FROM nullable"); err != nil {
+		t.Fatalf("err while clearing nullable table: %s", err.Error())
+	}
+}
+
+func dummy(now time.Time) *nullable {
+	return &nullable{
+		StringNVal: String("string_n_val"),
+		StringVal:  "string_val",
+
+		Int64NVal: Int64(123),
+
+		Int64Val: int64(123),
+
+		Float64NVal: Float64(12),
+		Float64Val:  float64(12),
+
+		BoolNVal: Bool(true),
+		BoolVal:  true,
+
+		TimeNVal: Time(now),
+		TimeVal:  now,
+	}
+}
+func TestMarshal(t *testing.T) {
+	now := time.Now().UTC()
+	nset := dummy(now)
+
+	allSetRes, err := json.Marshal(nset)
+	if err != nil {
+		t.Fatalf("err while marshaling: %s", err.Error())
+	}
+
+	// test all-set variables marshaling
+	allSetExpectedResString := fmt.Sprintf(
+		`{"StringNVal":"string_n_val","StringVal":"string_val","Int64NVal":123,"Int64Val":123,"Float64NVal":12,"Float64Val":12,"BoolNVal":true,"BoolVal":true,"TimeNVal":"%s","TimeVal":"%s"}`,
+		now.Format(time.RFC3339Nano),
+		now.Format(time.RFC3339Nano),
+	)
+	if allSetExpectedResString != string(allSetRes) {
+		t.Fatalf("Marshal err: expected: %s, got: %s", allSetExpectedResString, string(allSetRes))
+	}
+
+	// test not-set variables marshalling
+	nnonset := &nullable{}
+	nonSetRes, err := json.Marshal(nnonset)
+	if err != nil {
+		t.Fatalf("err while marshaling:%s", err.Error())
+	}
+
+	nonSetExpectedResString := fmt.Sprintf(
+		`{"StringNVal":null,"StringVal":"","Int64NVal":null,"Int64Val":0,"Float64NVal":null,"Float64Val":0,"BoolNVal":null,"BoolVal":false,"TimeNVal":null,"TimeVal":"%s"}`,
+		zeroDate().Format(time.RFC3339Nano),
+	)
+	if nonSetExpectedResString != string(nonSetRes) {
+		t.Fatalf("Marshal err: expected: %s, got: %s", nonSetExpectedResString, string(nonSetRes))
+	}
+}
+
+func TestUnMarshal(t *testing.T) {
+	now := time.Now().UTC()
+	nset := dummy(now)
+
+	allSetRes, err := json.Marshal(nset)
+	if err != nil {
+		t.Fatalf("err while marshaling: %s", err.Error())
+	}
+
+	// test not-set variables marshalling
+	nnonset := &nullable{}
+	nonSetRes, err := json.Marshal(nnonset)
+	if err != nil {
+		t.Fatalf("err while marshaling:%s", err.Error())
+	}
+
+	// test all set variables unmarshalling
+	nset2 := &nullable{}
+	if err := json.Unmarshal(allSetRes, nset2); err != nil {
+		t.Fatalf("Unmarshal err: %s", err.Error())
+	}
+
+	if !reflect.DeepEqual(nset, nset2) {
+		t.Fatalf("not same: \nn:%#v,\nn2:%#v", nset, nset2)
+	}
+
+	// test not-set variables unmarshaling
+	nnonset2 := &nullable{}
+	if err := json.Unmarshal(nonSetRes, nnonset2); err != nil {
+		t.Fatalf("Unmarshal err: %s", err.Error())
+	}
+
+	// deep equal fails on zerodate's loc property, check it manually
+	if nnonset.TimeVal.UnixNano() != nnonset2.TimeVal.UnixNano() {
+		t.Fatalf("time marshaling is not correct")
+	}
+
+	nnonset.TimeVal = zeroDate()
+	nnonset2.TimeVal = zeroDate()
+
+	if !reflect.DeepEqual(nnonset, nnonset2) {
+		t.Fatalf("not same: \nn:%#v,\nn2:%#v", nnonset, nnonset2)
+	}
+}
+
+func TestMarshalNullTime(t *testing.T) {
+	nt := &NullTime{}
+	err := nt.UnmarshalJSON([]byte("null1"))
+	if err == nil {
+		t.Fatal("null1 is not a valid time")
+	}
+}
+
+func TestMarshalNullBool(t *testing.T) {
+	nb := &NullBool{}
+	err := nb.UnmarshalJSON([]byte("null1"))
+	if err == nil {
+		t.Fatal("null1 is not a valid bool")
+	}
+}
+
+func zeroDate() time.Time {
+	return time.Date(1, time.January, 1, 0, 0, 0, 0, time.FixedZone("", 0))
 }
